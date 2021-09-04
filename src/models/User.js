cb9c8deaa34d464b82508b595c3a96f6bd165b36
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2021-09-04 11:55:32 +0530

    email validator regex

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,12 @@ const productSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: {
+      type: String,
+      /* regex to validate the email address provided */
+      match:
+        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    },
     password: { type: String, required: true },
   },
   {
